df07cd31390823eb6d4cac3c2c6382109d7e614b
Caio Rocha <cw.rocha7@gmail.com>
2019-04-04 00:04:37 -0300

    configuration file for webpack

---
+++ react-webpack/webpack.config.js
@@ -0,0 +1,14 @@
+'use strict'
+
+const path = require('path')
+
+//__dirname serve reconhecer o sistema operacional e utilizar a barra correta para cada sistema.
+//entry é o arquivo de entrada para o bundle.
+//output é o arquivo gerado após o bundle a partir de entry.
+module.exports = {
+	entry: path.join(__dirname, 'src', 'index'),
+	output: {
+		path: path.join(__dirname, 'dist'),
+		filename: 'bundle.js'
+	}
+}
