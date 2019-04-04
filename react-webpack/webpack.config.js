d65885a9b66ffb874600a2f7168630120b581e14
Caio Rocha <cw.rocha7@gmail.com>
2019-04-04 00:26:59 -0300

    adding a publicPath to the webserver of the webpack application

--- react-webpack/webpack.config.js
+++ react-webpack/webpack.config.js
@@ -5,10 +5,12 @@ const path = require('path')
 //__dirname serve reconhecer o sistema operacional e utilizar a barra correta para cada sistema.
 //entry é o arquivo de entrada para o bundle.
 //output é o arquivo gerado após o bundle a partir de entry.
+//publicPath é o local onde o webpack do webserver vai ficar assistindo o arquivo de desenvolvimento.
 module.exports = {
 	entry: path.join(__dirname, 'src', 'index'),
 	output: {
 		path: path.join(__dirname, 'dist'),
-		filename: 'bundle.js'
+		filename: 'bundle.js',
+		publicPath: '/static/'
 	}
 }
