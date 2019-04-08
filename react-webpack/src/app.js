87c6bb63582f7b96e6c9f6526fc10b245f3ad1d2
Caio Broflovski <cw_rocha@hotmail.com>
2019-04-08 13:40:42 -0300

    creating function soma and exporting with module.exports

---
+++ react-webpack/src/app.js
@@ -0,0 +1,7 @@
+'use strict'
+
+function soma(va1, va2){
+	return va1 + va2
+}
+
+module.exports = soma
--- react-webpack/src/index.js
+++ react-webpack/src/index.js
@@ -1,1 +1,6 @@
+'use strict'
+
+var sum = require('./app') //sum é a variável que recebe o import da função em app.js
+
+console.log(sum(2, 2)) //sum não é o nome da função em app, mas sim da variável que recebeu o import
 console.log('webpack está funcionando')
