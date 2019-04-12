4563689497ce38906c5ae865368347ee11f99019
Caio Broflovski <cw_rocha@hotmail.com>
2019-04-11 21:29:21 -0300

    importing react and react-dom from modules and rendering by reactDOM module. Sending the data for html by document.querySelector [data-js]

--- react-webpack/src/index.js
+++ react-webpack/src/index.js
@@ -1,6 +1,14 @@
 'use strict'
 
-var sum = require('./app') //sum é a variável que recebe o import da função em app.js
+//var sum = require('./app/soma') //sum é a variável que recebe o import da função em app.js
+var React = require('react')
+var ReactDOM = require('react-dom')
+var Title = require('./app')
 
-console.log(sum(2, 2)) //sum não é o nome da função em app, mas sim da variável que recebeu o import
+ReactDOM.render(
+  React.createElement(Title),
+  document.querySelector('[data-js]')
+)
+
+//console.log(sum(2, 2)) //sum não é o nome da função em app, mas sim da variável que recebeu o import
 console.log('webpack está funcionando')
